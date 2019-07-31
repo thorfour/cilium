838dfa80482e1ec321066dd4849f33a2fdf01506
Ian Vernon <ian@cilium.io>
2019-07-31 11:24:25 -0700

    datapath/maps: refactor to avoid pulling in endpointmanager dependency directly
    This allows for injection of the endpointmanager dependency into said package,
    as well as removes use of the global endpointmanager from the package.
    
    Signed-off by: Ian Vernon <ian@cilium.io>

--- daemon/daemon_main.go
+++ daemon/daemon_main.go
@@ -1402,8 +1402,12 @@ func runDaemon() {
 				<-restoreComplete
 			}
 			d.dnsNameManager.CompleteBootstrap()
-			maps.CollectStaleMapGarbage()
-			maps.RemoveDisabledMaps()
+
+			ms := maps.NewMapSweeper(&EndpointMapManager{
+				endpointManager: d.endpointManager,
+			})
+			ms.CollectStaleMapGarbage()
+			ms.RemoveDisabledMaps()
 		}()
 	}
 
